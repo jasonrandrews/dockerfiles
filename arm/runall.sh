b92e071d99e6e27a1e9a61d6ca7e445a1fe5710a
Jason Andrews <jason.r.andrews@comcast.net>
2019-04-17 12:00:51 -0500

    test script

---
+++ arm/runall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+exec > >(tee -a "logfile") 2>&1
+
+echo "running all tests"
+
+for dtest in alpine golang ruby rust ubuntu ubuntu-uname ; do
+  cd $dtest/v7 
+  ./build.sh
+  cd ../v8
+  ./build.sh
+  cd ../..
+done
