5c34b8160fcf774a81e3ed9441377ab9525e92bb
Jason Andrews <jason.r.andrews@comcast.net>
2019-04-17 12:53:05 -0500

    test script

--- arm/runall.sh
+++ arm/runall.sh
@@ -4,7 +4,7 @@ exec > >(tee -a "logfile") 2>&1
 
 echo "running all tests"
 
-for dtest in alpine golang ruby rust ubuntu ubuntu-uname python2 ; do
+for dtest in alpine golang ruby rust ubuntu ubuntu-uname python2 python3 ; do
   cd $dtest/v7 
   ./build.sh
   cd ../v8
